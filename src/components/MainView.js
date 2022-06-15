d8ad6944b9b1fc44cf23cbf46be9405c054b891f
Deniz Abdurrahmani <abdurrahmani.deniz00@gmail.com>
2022-06-16 01:58:17 +0300

    Added Run query button

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,4 +1,5 @@
-import {GridItem} from "@chakra-ui/react";
+import {TriangleUpIcon} from "@chakra-ui/icons";
+import {Button, GridItem} from "@chakra-ui/react";
 import React from "react";
 import ZoomControl from "./ZoomControl";
 
@@ -12,6 +13,20 @@ const MainView = () => {
         position="relative"
       >
         <ZoomControl />
+        <Button
+          bg="#FF5454"
+          color="#fff"
+          size="lg"
+          fontSize="md"
+          position="absolute"
+          right="8"
+          bottom="8"
+          _hover={{
+            bg: "#D1464E",
+          }}
+        >
+          <TriangleUpIcon transform="rotate(90deg)" marginRight={2} /> Run query
+        </Button>
       </GridItem>
     </>
   );
