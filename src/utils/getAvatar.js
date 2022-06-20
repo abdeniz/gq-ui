2626eb13ecd267c860fcf8365c547e09c88a7aa6
Deniz Abdurrahmani <abdurrahmani.deniz00@gmail.com>
2022-06-20 10:31:08 +0300

    More fixes

--- src/utils/getAvatar.js
+++ src/utils/getAvatar.js
@@ -1,13 +1,13 @@
 import {supabase} from "./supabaseClient";
 
 export default async function getAvatar(user) {
-  const {data} = await supabase
+  const {data, error} = await supabase
     .from("profiles")
     .select(`avatar_url`)
     .eq("id", user?.id)
     .single();
 
-  if (!data || !data.avatar_url) {
+  if (error || !data.avatar_url) {
     return user.user_metadata.avatar_url;
   }
 
